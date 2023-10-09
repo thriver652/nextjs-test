100778e72f214ffbd81b976be51b069c3e0de6c5
Sai Praneeth Talluri <saipraneeth@Sais-MacBook-Air.local>
2023-10-10 04:14:45 +0530

    Basic Component Declaration

---
+++ components/UserActivity.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const UserActivity = () => {
+  return (
+    <div>
+      <button onClick={()=>console.log("Hello")} > Go to User Activity Page!</button>
+    </div>
+  )
+}
+
+export default UserActivity
